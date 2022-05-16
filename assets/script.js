68dc005f5bd2bd33c5c18776b6a3a4c4b82b2ddf
Sonny Addison <sonny.m.addison@hotmail.com>
2022-05-15 18:50:15 -0700

    Just throwing things at the wall to see what will work

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password");
 
 /* Password Content */
 var content  = {
@@ -9,49 +10,60 @@ var content  = {
   numberic: ["1234567890"],
   options: "",
 }
-//Questions that will be asked //
 
-var length = prompt("How many Characters do you want? Min 8 Max 128");
-var upper = confirm("Would you like Upper Case Letters?");
-var lower = confirm("Would you like Lower Case Letters?");
-var special = confirm("Would you like Special Characters?");
-var num =  confirm("Would you like numbers?");
+// Making random password //
 
-//Making sure all enough information is there//
+function start() {
+  // Making sure that the system does not work before questions asked //
+  event.preventDefault();
+  
+  //Making a holding area for the random information//
+  var randomPassword = "";
 
-if(length < 8 || > 128) {
-  alert("Please select correct amount of Characters.");
-  var length = prompt("How many Characters do you want? Min 8 Max 128");
-}
-else if (upper != true && lower !=true && special !=true && number !=true) {
-  alert("Please select at least two varables for password, thank you.");
+  //Questions that will be asked //
   var length = prompt("How many Characters do you want? Min 8 Max 128");
   var upper = confirm("Would you like Upper Case Letters?");
   var lower = confirm("Would you like Lower Case Letters?");
   var special = confirm("Would you like Special Characters?");
-  var num = confirm("Would you like numbers?")
-}
+  var num =  confirm("Would you like numbers?");
 
-//Password to allow for selctions//
-if (upper) {
-  content.options += content.upperCase;
-} 
-if (lower) {
-  content.options += content.lowerCase;
-}
-if (special) {
-  content.options += content.specialChararacters;
-}
-if (num) {
-  content.options += content.numberic;
-}
+  //Making sure all enough information is there//
+  if(length < 8 || > 128) {
+    alert("Please select correct amount of Characters.");
+    var length = prompt("How many Characters do you want? Min 8 Max 128");
+  }
+  else if (upper != true && lower !=true && special !=true && number !=true) {
+    alert("Please select at least two varables for password, thank you.");
+    var length = prompt("How many Characters do you want? Min 8 Max 128");
+    var upper = confirm("Would you like Upper Case Letters?");
+    var lower = confirm("Would you like Lower Case Letters?");
+    var special = confirm("Would you like Special Characters?");
+    var num = confirm("Would you like numbers?")
+  }
 
-//Magic happens with random password using what was selcect//
-for (var 1= 0; i < length; i++) {
-  content.options.charAt(Math.floor(Math.random()*content.options.length));
+  //Password to allow for selctions//
+  if (upper) {
+    content.options += content.upperCase;
+  } 
+  if (lower) {
+    content.options += content.lowerCase;
+  }
+  if (special) {
+    content.options += content.specialChararacters;
+  }
+  if (num) {
+    content.options += content.numberic;
+  }
+
+  //Magic happens with random password using what was selcect//
+  for (var 1= 0; i < length; i++) {
+    randomPassword += content.options.charAt(Math.floor(Math.random()*content.options.length));
+  }
+  password.value = randomPassword;
 }
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -59,5 +71,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-
 generateBtn.addEventListener("click", writePassword);
