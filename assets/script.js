c2b0c72f5a68d350ac27c0e5ba114c6ac32e3b3f
Sonny Addison <sonny.m.addison@hotmail.com>
2022-05-15 18:30:46 -0700

    Tried to make obstical with questions

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,63 @@
 var generateBtn = document.querySelector("#generate");
 
 /* Password Content */
-var content = {
-    upperCase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-    lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    specialChararacters: ["!", "@", "#", "$", "%", "^", "&"],
-    numberic: ["1", "2", "3", "4","5","6","7", "8", "9", "0"],
-    }
-     
+var content  = {
+  upperCase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+  lowerCase: ["abcdefghijklmnopqrstuvwxyz"],
+  specialChararacters: ["!@#$%^&*"],
+  numberic: ["1234567890"],
+  options: "",
+}
+//Questions that will be asked //
+var = questions {
+length: prompt("How many Characters do you want? Min 8 Max 128"),
+upper: confirm("Would you like Upper Case Letters?"),
+lower: confirm("Would you like Lower Case Letters?"),
+special: confirm("Would you like Special Characters?"),
+num: confirm("Would you like numbers?"),
+}
+
+//Making sure all enough information is there//
+
+if(length < 8 || > 128) {
+  alert("Please select correct amount of Characters.");
+  var length = prompt("How many Characters do you want? Min 8 Max 128");
+}
+else if (upper != true && lower !=true && special !=true && number !=true) {
+  alert("Please select at least two varables for password, thank you.");
+  var length = prompt("How many Characters do you want? Min 8 Max 128");
+  var upper = confirm("Would you like Upper Case Letters?");
+  var lower = confirm("Would you like Lower Case Letters?");
+  var special = confirm("Would you like Special Characters?");
+  var num = confirm("Would you like numbers?")
+}
+
+//Password to allow for selctions//
+if (upper) {
+  content.options += content.upperCase;
+} 
+if (lower) {
+  content.options += content.lowerCase;
+}
+if (special) {
+  content.options += content.specialChararacters;
+}
+if (num) {
+  content.options += content.numberic;
+}
+
+//Magic happens with random password using what was selcect//
+for (var 1= 0; i < length; i++) {
+  content.options.charAt(Math.floor(Math.random()*content.options.length));
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
